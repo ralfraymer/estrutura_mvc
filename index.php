fc1bae5e8f35a0efdc3a470267b6b828587f7a47
Ralf Raymer Will <ralfraymer@gmail.com>
2020-04-13 23:49:09 -0300

    Important! -> The index.php
    In index.php is autoload.php, I want create file individual for this file, due the organization of the future projects.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require 'config.php'; 
+
+spl_autoload_register(function($class){
+
+     if(file_exists('controllers/'.$class.'.class.php')){
+         require 'controllers/'.$class.'.class.php';
+     }else if(file_exists('models/'.$class.'.class.php')){
+        require 'models/'.$class.'.class.php';
+     } else if(file_exists('core/'.$class.'.class.php')){
+        require 'core/'.$class.'.class.php';
+    };
+
+
+});
+
+$core = new Core(); 
+
+$core->run();
+?>
