eb06d19f8808bdfd72b2671a16dce8d44f367811
Ralf Raymer Will <ralfraymer@gmail.com>
2020-04-15 13:14:40 -0300

    Update Htaccess

--- core/Core.class.php
+++ core/Core.class.php
@@ -8,18 +8,26 @@ class Core{
              $url .= $_GET['url'];
         }
 
-        if(empty($url) && $url != '/'){
+        if(!empty($url) && $url != '/'){
             $url = explode('/', $url);
             array_shift($url);
 
             $currentController = $url[0].'Controller';  
+            array_shift($url);
+            
+            if(isset($url[0]) && !empty($url[0])){
+                $currentAction = $url[0];
+            } else{
+                $currentAction = 'index';
+            }
         } else{
             $currentController = 'homeController';
             $currentAction = 'index';
         }
-        echo "URL: ".$url;
-        echo "Controler: ".$currentController;
-        echo "Action: ".$currentAction;
+
+
+        echo "</br>Controler: ".$currentController;
+        echo "</br>Action: ".$currentAction;
 
     }
 }
--- index.php
+++ index.php
@@ -6,9 +6,11 @@ spl_autoload_register(function($class){
 
      if(file_exists('controllers/'.$class.'.class.php')){
          require 'controllers/'.$class.'.class.php';
-     }else if(file_exists('models/'.$class.'.class.php')){
+     }
+     else if(file_exists('models/'.$class.'.class.php')){
         require 'models/'.$class.'.class.php';
-     } else if(file_exists('core/'.$class.'.class.php')){
+     } 
+     else if(file_exists('core/'.$class.'.class.php')){
         require 'core/'.$class.'.class.php';
     };
 
